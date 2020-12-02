b209c5304e3d0c41b9f3a192b93ea2d45e0a0be3
Hleb Pashkevich <hleb_pashkevich@epam.com>
2020-12-02 21:46:56 +0300

    renaming of 'My Test'

--- test/airport.spec.js
+++ test/airport.spec.js
@@ -9,7 +9,7 @@ const experimentalPlane = require('../Planes/experimentalPlane');
 const ExperimentalTypes = require('../models/ExperimentalTypes');
 const ClassificationLevel = require('../models/ClassificationLevel');
 
-describe('My Test', () => {
+describe('Plane With Max Passenger Capacity', () => {
 
     let planes = [
         new PassengerPlane('Boeing-737', 900, 12000, 60500, 164),
